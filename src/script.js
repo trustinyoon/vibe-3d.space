8a74e45c71f35be5e49a8481efde56e94d119541
trustinyoon <trustinyoon@gmail.com>
2021-07-14 03:03:22 -0700

    updated galaxy phrase

--- src/script.js
+++ src/script.js
@@ -13,21 +13,29 @@ import { Material } from 'three'
 
 const gui = new dat.GUI();
 
-const parameters = {
-  color: 0x412f2f
-};
 
-gui
-  .addColor(parameters, 'color')
-  .onChange(() => {
-    floorMaterial.color.set(parameters.color);
-  });
+
 
 //--------------------------------Textures--------------------------------//
 
   // Loading Manager Setup
 const loadingManager = new THREE.LoadingManager();
 const textureLoader = new THREE.TextureLoader(loadingManager);
+
+  // Ice
+const iceColorTexture = textureLoader.load('/ice/Ice_001_COLOR.jpg');
+const iceNormalTexture = textureLoader.load('/ice/Ice_001_NRM.jpg');
+const iceOccTexture = textureLoader.load('/ice/Ice_001_OCC.jpg');
+const iceSpecTexture = textureLoader.load('/ice/Ice_001_SPEC.jpg');
+const iceHeightTexture = textureLoader.load('/ice/Ice_001_DISP.png');
+
+  // Metal
+const metalColorTexture = textureLoader.load('/metal/Metal_Plate_Sci-Fi_001_basecolor.jpg');
+const metalAoTexture = textureLoader.load('/metal/Metal_Plate_Sci-Fi_001_ambientOcclusion.jpg');
+const metalHeightTexture = textureLoader.load('/metal/Metal_Plate_Sci-Fi_001_height.png');
+const metalMetallicTexture = textureLoader.load('/metal/Metal_Plate_Sci-Fi_001_metallic.jpg');
+const metalNormalTexture = textureLoader.load('/metal/Metal_Plate_Sci-Fi_001_normal.jpg');
+const metalRoughnessTexture = textureLoader.load('/metal/Metal_Plate_Sci-Fi_001_roughness.jpg');
 
 loadingManager.onStart = () => {
   console.log('onStart');
@@ -45,34 +53,6 @@ loadingManager.onError = () => {
   console.log('onError');
 }
 
-  // Floor Textures
-const floorColorTexture = textureLoader.load('/floor/Wood_Floor_009_basecolor.jpg');
-const floorAmbientOcclusionTexture = textureLoader.load('/floor/Wood_Floor_009_ambientOcclusion.jpg');
-const floorNormalTexture = textureLoader.load('/floor/Wood_Floor_009_normal.jpg');
-const floorRoughnessTexture = textureLoader.load('/floor/Wood_Floor_009_roughness.jpg');
-
-  // Ceiling Textures
-const ceilingColorTexture = textureLoader.load('/ceiling/Ceiling_Drop_Tiles_001_basecolor.jpg');
-const ceilingAmbientOcclusionTexture = textureLoader.load('/ceiling/Ceiling_Drop_Tiles_001_ambientOcclusion.jpg');
-const ceilingHeightTexture = textureLoader.load('/ceiling/Ceiling_Drop_Tiles_001_height.png')
-const ceilingRoughnessTexture = textureLoader.load('/ceiling/Ceiling_Drop_Tiles_001_roughness.jpg');
-const ceilingNormalTexture = textureLoader.load('/ceiling/Ceiling_Drop_Tiles_001_normal.jpg');
-
-  // Wall Textures
-const wallColorTexture = textureLoader.load('/carrara_marble/Marble_Carrara_002_COLOR.jpg');
-const wallAmbientOcclusionTexture = textureLoader.load('/carrara_marble/Marble_Carrara_002_OCC.jpg');
-const wallNormalTexture = textureLoader.load('/carrara_marble/Marble_Carrara_002_NORM.jpg');
-const wallRoughnessTexture = textureLoader.load('/carrara_marble/Marble_Carrara_002_ROUGH.jpg');
-
-  // Window Textures
-const windowColorTexture = textureLoader.load('/window/Window_001_basecolor.jpg');
-const windowAmbientOcclusionTexture = textureLoader.load('/window/Window_001_ambientOcclusion.jpg');
-const windowNormalTexture = textureLoader.load('/window/Window_001_normal.jpg');
-const windowRoughnessTexture = textureLoader.load('/window/Window_001_roughness.jpg');
-const windowMetallicTexture = textureLoader.load('/window/Window_001_metallic.jpg');
-const windowHeightTexture = textureLoader.load('/window/Window_001_height.png');
-const windowOpacityTexture = textureLoader.load('/window/Window_001_opacity.jpg');
-
 //-----------------------------------Font----------------------------------->>
 
 const fontLoader = new THREE.FontLoader();
@@ -84,24 +64,38 @@ fontLoader.load(
       'vibe-3d.space',
       {
         font: font,
-        size: 0.5,
-        height: 0.2,
+        size: .8,
+        height: 0.3,
         curveSegments: 12,
         bevelEnabled: true,
-        bevelThickness: 0.03,
-        bevelSize: 0.02,
+        bevelThickness: .7,
+        bevelSize: 0.1,
         bevelOffset: 0,
-        bevelSegments: 5
+        bevelSegments: 1,
+        wireframe: false
       }
     )
 
-    const textMaterial = new THREE.MeshStandardMaterial({
-      normalMap: floorNormalTexture
+    textGeometry.center();
 
+    const textMaterial = new THREE.MeshStandardMaterial({
+      metalness: .2,
+      roughness: .3
     });
+
     const text = new THREE.Mesh(textGeometry, textMaterial);
-    
+    text.geometry.setAttribute(
+      'uv2',
+      new THREE.BufferAttribute(text.geometry.attributes.uv.array, 2)
+    );
+    console.log(text)
     scene.add(text);
+
+    // debugger
+    const textFolder = gui.addFolder("Text");
+    textFolder.add(textMaterial, 'metalness', 0, 1, .01);
+    textFolder.add(textMaterial, 'roughness', 0, 1, .01);
+    // textFolder.add(textMaterial)
   }
 )
 
@@ -112,59 +106,12 @@ const canvas = document.querySelector('canvas.webgl');
 //-----------------------------------Scene----------------------------------->>
 
 const scene = new THREE.Scene();
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 const gridHelper = new THREE.GridHelper(10, 20);
-// scene.add(gridHelper)
+scene.add(gridHelper)
 
-//---------------------------------Geometries--------------------------------->>
-
-const floorGeometry = new THREE.PlaneGeometry(12, 11.5);
-const wallGeometry = new THREE.PlaneGeometry(10, 5);
-const sideWindowGeometry = new THREE.PlaneGeometry(5, 5);
-const mainWindowGeometry = new THREE.PlaneGeometry(5, 5);
-
-//---------------------------------Materials--------------------------------->>
-
-  // Floor Materials
-const floorMaterial = new THREE.MeshStandardMaterial(
-  { 
-    // specular: new THREE.Color(0xff0000),
-    map: floorColorTexture,
-    normalMap: floorNormalTexture,
-    roughnessMap: floorRoughnessTexture,
-    aoMap: floorAmbientOcclusionTexture,
-    aoMapIntensity: 10,
-    // displacementMap: floorHeightTexture,
-    // displacementScale: .1,
-    side: THREE.DoubleSide
-  });
-
-gui
-  .add(floorMaterial, 'metalness').min(0).max(1).step(.0001);
-  
-gui
-  .add(floorMaterial, 'roughness').min(0).max(1).step(.0001);
-
-  // Ceiling Materials
-const ceilingMaterial = new THREE.MeshStandardMaterial({
-  side: THREE.DoubleSide,
-  map: ceilingColorTexture,
-  normalMap: ceilingNormalTexture,
-  roughnessMap: ceilingRoughnessTexture,
-  aoMap: ceilingAmbientOcclusionTexture,
-  aoMapIntensity: 2
-});
-
-  // Walls Materials
-const wallMaterial = new THREE.MeshStandardMaterial({
-  map: wallColorTexture,
-  side: THREE.DoubleSide,
-  normalMap: wallNormalTexture,
-  aoMap: wallAmbientOcclusionTexture,
-  aoMapIntensity: 1,
-  roughnessMap: wallRoughnessTexture
-});
-
-  // Skybox
+// Skybox
 const skyboxLoader = new THREE.CubeTextureLoader();
 const skyboxTexture = skyboxLoader.load([
   '/spaceBox/px.png',
@@ -177,78 +124,16 @@ const skyboxTexture = skyboxLoader.load([
 
 scene.background = skyboxTexture;
 
-  // Window Materials
-const sideWindowMaterial = new THREE.MeshStandardMaterial({
-  map: windowColorTexture,
-  side: THREE.DoubleSide,
-  normalMap: windowNormalTexture,
-  aoMap: windowAmbientOcclusionTexture,
-  aoMapIntensity: 5,
-  metalnessMap: windowMetallicTexture,
-  opacity: 100,
-  roughnessMap: windowRoughnessTexture,
-  alphaMap: windowOpacityTexture,
-});
+//---------------------------------Geometries--------------------------------->>
 
-const mainWindowMaterial = new THREE.MeshStandardMaterial({
-  transparent: true,
-  side: THREE.DoubleSide,
-  // opacity: 100,
-  metalnessMap: windowMetallicTexture,
-  roughness: 100,
-  // envMap: skyboxTexture,
-  // normalMap: windowNormalTexture
-});
-console.log(skyboxTexture)
+
+
+//---------------------------------Materials--------------------------------->>
+
+
+
 //-----------------------------------Meshes----------------------------------->>
 
-  // Floor Meshes
-const floor = new THREE.Mesh(floorGeometry, wallMaterial);
-floor.geometry.setAttribute(
-  'uv2', 
-  new THREE.BufferAttribute(floor.geometry.attributes.uv.array, 2)
-);
-floor.position.set(0, 0, -.75);
-floor.rotateX(Math.PI/2);
-
-  // Ceiling Meshes
-const ceiling = new THREE.Mesh(floorGeometry, wallMaterial);
-ceiling.position.set(0, 5, -.75);
-ceiling.rotateX(Math.PI / 2);
-ceiling.geometry.setAttribute(
-  'uv2',
-  new THREE.BufferAttribute(floor.geometry.attributes.uv.array, 2)
-);
-
-  // Wall Meshes
-const wall1 = new THREE.Mesh(wallGeometry, wallMaterial);
-wall1.position.set(-5, 2.5, 0);
-wall1.rotateY(Math.PI / 2);
-wall1.geometry.setAttribute(
-  'uv2',
-  new THREE.BufferAttribute(floor.geometry.attributes.uv.array, 2)
-);
-const wall2 = new THREE.Mesh(wallGeometry, wallMaterial);
-wall2.position.set(5, 2.5, 0);
-wall2.rotateY(Math.PI / 2);
-
-  // Window Meshes
-const sideWindow = new THREE.Mesh(sideWindowGeometry, sideWindowMaterial);
-sideWindow.position.set(-4.95, 3.5, 0);
-sideWindow.rotateY(Math.PI / 2);
-sideWindow.scale.set(.5, .25);
-
-const mainWindow = new THREE.Mesh(mainWindowGeometry, mainWindowMaterial);
-mainWindow.position.set(0, 2.5, -6.5);
-
-  // Adding meshes to the room Group
-const room = new THREE.Group();
-room.add(floor);
-room.add(wall1);
-room.add(wall2);
-room.add(ceiling);
-room.add(mainWindow);
-// scene.add(room);
 
 //-----------------------------------Lights----------------------------------->>
 
