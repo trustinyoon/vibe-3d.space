2cc0f362d22bfd158c412967418e8d53d008fd4a
trustinyoon <trustinyoon@gmail.com>
2021-08-31 17:37:58 +0900

    added spin

--- src/script.js
+++ src/script.js
@@ -113,7 +113,9 @@ const parameters = {
   count: 1000,
   size: .02,
   radius: 5,
-  branches: 3
+  branches: 3,
+  spin: 1,
+  randomness: .2
 }
 
 let galaxyGeometry = null;
@@ -136,11 +138,16 @@ const generateGalaxy = () => {
     const i3 = i * 3
   
     const radius = Math.random() * parameters.radius;
+    const spinAngle = radius * parameters.spin;
     const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2;
-    
-    positions[i3 + 0] = Math.cos(branchAngle) * radius;
-    positions[i3 + 1] = 0
-    positions[i3 + 2] = Math.sin(branchAngle) * radius;
+
+    const randomX = (Math.random() - 0.5) * parameters.randomness * radius
+    const randomY = (Math.random() - 0.5) * parameters.randomness * radius
+    const randomZ = (Math.random() - 0.5) * parameters.randomness * radius
+
+    positions[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX
+    positions[i3 + 1] = randomY;
+    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
   }
 
   // Geometry
@@ -168,6 +175,8 @@ gui.add(parameters, "count").min(100).max(1000000).step(100).onFinishChange(gene
 gui.add(parameters, "size").min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, "radius").min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy);
 gui.add(parameters, "branches").min(2).max(20).step(1).onFinishChange(generateGalaxy);
+gui.add(parameters, "spin").min(-5).max(5).step(.001).onFinishChange(generateGalaxy);
+gui.add(parameters, "randomness").min(0).max(2).step(.001).onFinishChange(generateGalaxy);
 
 //---------------------------------Geometries--------------------------------->>
 
